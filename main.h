a910570fbd111b70210afefc0c037af4e35a8d29
sentense1 <3xsaint@gmail.com>
2023-02-17 05:13:34 -0800

    10%

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ int _printf(const char *format, ...);
 int _write_char(char);
 int print_char(va_list);
 int print_string(va_list);
-int print_percernt(va_list);
+int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
 int print_binary(va_list);

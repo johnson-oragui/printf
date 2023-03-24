8ad2fae105c7521a7123f06ee6fa11c19f92ab4c
sentense1 <3xsaint@gmail.com>
2023-03-24 11:15:36 -0700

    printf

--- handlers.c
+++ handlers.c
@@ -161,6 +161,8 @@ int handle_precision(va_list args, const char *modifier, char *index)
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
  * @specifier: A pointer to a potential conversion specifier.
+ * @int: integer
+ * @char: character
  *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
